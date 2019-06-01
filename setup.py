0d3c7555828c38e97db1111ecd41d0d0a4e29030
guldfisk <ce.guldfisk@gmail.com>
2019-06-01 15:51:52 +0200

    yikes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def package_files(directory):
 			paths.append(os.path.join('..', path, filename))
 	return paths
 
-extra_files = package_files('sheetclient')
+extra_files = package_files('mkmcheck')
 
 setup(
 	name='mkmcheck',
